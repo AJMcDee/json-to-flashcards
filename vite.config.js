f7e44128a8bca7fa37a510cbbb50f9c239f9582a
Anna McDougall <ajmcdougall@Annas-MBP.fritz.box>
2026-01-21 19:41:28 +0100

    Add build folder for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/json-to-flashcards/',
   test: {
     environment: 'jsdom',
     globals: true,
